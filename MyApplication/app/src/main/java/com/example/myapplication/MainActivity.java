df2e8d51954fbafd4c2770995c2a6130f220a65c
Stefanos Ifoulis <stefanoy@csd.auth.gr>
2023-05-07 11:54:59 +0300

    Project added

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button greetButton = findViewById(R.id.greet_button);
+        TextView helloText = findViewById(R.id.hello_textview);
+
+        greetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Result.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/Result.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+
+        Button okButton = findViewById(R.id.ok_button);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Result.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
